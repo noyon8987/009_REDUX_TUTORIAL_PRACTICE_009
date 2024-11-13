dadded0c1269201c3d6c196f15c3152291a0f14a
Noyon-Tech-Solution <shahadathosen8987@gmail.com>
2024-11-13 19:45:17 +0600

    Noyon Tech Solution

---
+++ Redux-06/index.js
@@ -0,0 +1,49 @@
+async function name(params) {
+    console.log("Hello World")
+}
+await name()
+
+function hello () {
+
+}
+hello()
+
+let hablu = () => {
+
+}
+hablu()
+
+import React from 'react'
+
+export default function nakom() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+import React from 'react'
+
+const vindex = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default vindex
+
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
